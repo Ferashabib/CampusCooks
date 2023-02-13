82c0506152c734cc5fcaca458188eb7e1f72c901
Huaqiang Ye <huaqiangy16@g.ucla.edu>
2023-02-12 16:44:37 -0800

    Multiple pages
    I added multiple pages for our web app and some additional style like background color.

---
+++ cs35w-app/src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+    return (
+      <div>
+        <h1>
+            Welcome to our about page. This app allows you to 
+            learn different recipes or upload your recipes.
+        </h1>
+      </div>
+    );
+};
+
+export default About;
---
+++ cs35w-app/src/pages/upload.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+  
+const Upload = () => {
+  return (
+    <div>
+      <h1>Upload your recipes here!</h1>
+    </div>
+  );
+};
+  
+export default Upload;
