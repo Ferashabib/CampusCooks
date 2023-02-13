ffbc36e947c86706c513cb5e288daff9aab8b2df
Huaqiang Ye <huaqiangy16@g.ucla.edu>
2023-02-12 21:53:50 -0800

    Multiple pages update
    Now the codes no longer needs the package "react-router-dom"

--- cs35w-app/src/App.js
+++ cs35w-app/src/App.js
@@ -1,23 +1,34 @@
+import React from "react";
 import './App.css';
+
+import './components/NavbarElement.css';
+import Route from './components/Router';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import Home from './pages';
 import About from './pages/about';
 import Login from './pages/log_in';
 import Upload from './pages/upload';
 
+
+
 function App() {
+  console.log(window.location.pathname);
   return (
     <div className='App'>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home/>} />
-          <Route path='/about' element={<About/>} />
-          <Route path='/log_in' element={<Login/>} />
-          <Route path='/upload' element={<Upload/>} />
-        </Routes>
-      </Router>
+      <Navbar/>
+      <Route path="/">
+        <Home/>
+      </Route>
+      <Route path="/about">
+        <About/>
+      </Route>
+      <Route path="/log_in">
+        <Login/>
+      </Route>
+      <Route path="/upload">
+        <Upload/>
+      </Route>
     </div>
   );
 }
---
+++ cs35w-app/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './NavbarElement.css'
+function Navbar(){
+    return (
+        <header>
+            <div className="Webname"> Name of our app </div>
+            <a href="/about" className="NavLink">
+                About
+            </a>
+            <a href="/log_in" className="NavLink">
+                Log in
+            </a>
+            <a href="/upload" className="NavLink">
+                Upload
+            </a>
+            <a href="/" className="NavLink">
+                Home
+            </a>
+        </header>
+    )
+}
+
+export default Navbar
