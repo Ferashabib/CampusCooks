b6af09c37c432a3438a01c7d494cee671e67a1b0
PanFishZ <yushanmeyers@gmail.com>
2023-02-14 17:59:44 -0800

    Images and LogIn Form added

--- cs35w-app/src/App.js
+++ cs35w-app/src/App.js
@@ -11,43 +11,23 @@ import Login from './pages/log_in';
 import Upload from './pages/upload';
 
 
-
 function App() {
   console.log(window.location.pathname);
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-      </a>
-	  <h1>This is Rin's branch</h1>
-      </header>
-=======
     <div className='App'>
-      <Navbar/>
+      <Navbar />
       <Route path="/">
-        <Home/>
+        <Home />
       </Route>
       <Route path="/about">
-        <About/>
+        <About />
       </Route>
       <Route path="/log_in">
-        <Login/>
+        <Login />
       </Route>
       <Route path="/upload">
-        <Upload/>
+        <Upload />
       </Route>
->>>>>>> ffbc36e947c86706c513cb5e288daff9aab8b2df
     </div>
   );
 }
