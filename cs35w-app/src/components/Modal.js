a7502e20ebab5e182e920eddda957ee996bf669b
PanFishZ <yushanmeyers@gmail.com>
2023-02-15 23:02:15 -0800

    Removed BackDrop and fixed fav & upvote buttons

--- cs35w-app/src/components/Modal.js
+++ cs35w-app/src/components/Modal.js
@@ -1,4 +1,4 @@
-function Modal() {
+function Modal(props) {
 
     function favHandler() {
         console.log("fav!!!")
@@ -10,8 +10,14 @@ function Modal() {
         <div className='Modal'>
             <p>Salad recipes are my favorite way to showcase vibrant, in-season produce – fruits and veggies that are so good on their own that you don’t need to do much to make them into a delicious meal. On cold winter nights, I like to brighten up our dinner table with a big, colorful mix of root veggies and hearty greens. On beautiful, warm days when I’d rather be outside than in the kitchen, summer salads are the perfect solution: they’re simple to toss together, but they’re totally delicious nonetheless.
             </p>
-            <button className="btn btn--alt" onClick={favHandler}>favorite</button>
-            <button className="btn" onClick={upvoteHandler}>Upvote</button>
+            <button className="btn btn--alt" onClick={() => {
+                favHandler();
+                props.onCancel();
+            }}>favorite</button>
+            <button className="btn" onClick={() => {
+                upvoteHandler();
+                props.onCancel();
+            }}>Upvote</button>
         </div>
     )
 
--- cs35w-app/src/components/Recipe.js
+++ cs35w-app/src/components/Recipe.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Modal from './Modal';
 import Backdrop from './Backdrop';
 
+
 function Recipe(props) {
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -13,11 +14,11 @@ function Recipe(props) {
         setModalIsOpen(false);
     }
 
+
+
     return (
         <div>
-            <div>{modalIsOpen && <Modal />}</div>
-            {/* Todo move backdrop to the back*/}
-            <div>{modalIsOpen && <Backdrop onCancel={closeModalHandler} />}</div>
+            <div>{modalIsOpen && <Modal onCancel={closeModalHandler} />} </div>
             <img src={require(`../images/${props.text}.jpg`)} alt={props.text} onClick={imageHandler} />
 
         </div >
