cdbc0c8905f804069bab096ee0abfe174d3c55f6
PanFishZ <yushanmeyers@gmail.com>
2023-02-22 17:52:01 -0800

    bug-fixed on password field

--- cs35w-app/src/components/LoginForm.js
+++ cs35w-app/src/components/LoginForm.js
@@ -2,14 +2,7 @@ import handleSubmit from '../handles/handlesubmit';
 import { useRef } from 'react';
 import { getDatabase, ref, set } from "firebase/database";
 
-// function writeUserData(userId, name, email, imageUrl) {
-//     const db = getDatabase();
-//     set(ref(db, 'users/' + userId), {
-//         username: name,
-//         email: email,
-//         profile_picture: imageUrl
-//     });
-// }
+// z
 
 function LogininForm(props) {
     //writeUserData("Henry", "email", "rain", "ewww")
