49bdfcb1a8784f7a97e01cd956b07fdc31ff96b3
Huaqiang Ye <huaqiangy16@g.ucla.edu>
2023-03-02 23:41:39 -0800

    Update UploadRecipes.js

--- cs35w-app/src/components/UploadRecipes.js
+++ cs35w-app/src/components/UploadRecipes.js
@@ -1,21 +1,35 @@
 import { db } from "../firebase_setup/firebase"
 import { addDoc, collection } from "@firebase/firestore"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
+
 
 function UploadRecipes() {
 
     function handleSubmit(e) {
         e.preventDefault();
         const ref = collection(db, "Upload");
-        let recipe = {
-            Recipe: document.getElementById('Recipe').value
-        }
-        try {
-            addDoc(ref, recipe)
-        } catch (err) {
-            console.log(err)
-        }
-        alert('Your recipe was submitted successfully! :D');
-        document.getElementById("uploadForm").reset();
+        onAuthStateChanged(auth, (user) => 
+        {
+            if (user) {
+                let recipe = {
+                    Recipe: document.getElementById('Recipe').value,
+                    UserName: user.email
+                }
+                try {
+                    addDoc(ref, recipe)
+                } catch (err) {
+                    console.log(err)
+                }
+                alert('Your recipe was submitted successfully! :D');
+                document.getElementById("uploadForm").reset();
+            } 
+            else {
+                alert("You must sign in before upload your recipe!");
+                document.getElementById("uploadForm").reset();
+            }
+        });
     };
 
     return (
@@ -23,10 +37,24 @@ function UploadRecipes() {
         <h1> Upload your recipe by filling the form here! </h1>
         <form className='cardTextArea' id="uploadForm">
         <div>
+<<<<<<< Updated upstream
             <label>
               Recipe: <br/>
             </label>
             <textarea id='Recipe' type='text' rows="10" cols="45" placeholder='Write your recipe here!'/>
+=======
+            <h1> Upload your recipe by filling the form here! </h1>
+            <h2> Note: You must log in before you can upload your recipe</h2>
+            <form className='cardTextArea' id="uploadForm">
+                <div>
+                    <label>
+                        Recipe: <br />
+                    </label>
+                    <textarea id='Recipe' type='text' rows="10" cols="45" placeholder='Write your recipe here!' />
+                </div>
+                <input className='btn' onClick={handleSubmit} type="submit" value="Submit" />
+            </form>
+>>>>>>> Stashed changes
         </div>
         <input className='btn' onClick={handleSubmit} type="submit" value="Submit" />
         </form>
