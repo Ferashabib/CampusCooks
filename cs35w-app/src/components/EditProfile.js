1d7f7a5e775b16a8877c9c2d1c0cf45e262227b4
patrickparseg <patrickparseg23@gmail.com>
2023-03-12 00:28:18 -0800

    changed navbar look and fixed typos

--- cs35w-app/src/components/EditProfile.js
+++ cs35w-app/src/components/EditProfile.js
@@ -81,7 +81,7 @@ const EditProfile = () => {
                 <div>
                     <label> Username: </label>
                     <input type="text" id="fname" name="fname"></input><br /><br />
-                    <label> FavoriteColor: </label>
+                    <label> Favorite Color: </label>
                     <input type="text" id="color" name="color"></input><br /><br />
                     <label> Birthday: </label>
                     <input type="text" id="birthday" name="birthday"></input><br /><br />
